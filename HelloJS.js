1aa14c03288d8851b2dc495ec0f56f4dd94f242e
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-05-06 22:25:40 +0530

    Learned Variable

--- HelloJS.js
+++ HelloJS.js
@@ -1,3 +1,11 @@
 console.log("Abhishek Upadhyay");
 console.log("Shankar Mobile");
 console.log("I love JS");
+
+Name ="Abhishek";
+console.log(Name);
+age = 21; 
+console.log(age);
+study="B.Sc";
+console.log(study);
+isFollow = false; //Boolean value
