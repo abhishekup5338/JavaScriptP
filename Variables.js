e576d482be16435f841fe2af15aa7c55b16acd5e
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-05-06 23:40:21 +0530

    Variables

---
+++ Variables.js
@@ -0,0 +1,26 @@
+
+Name ="Abhishek";
+console.log(Name);
+age = 21; 
+console.log(age);
+study="B.Sc";
+console.log(study);
+isFollow = false; //Boolean value
+totalPrice= 999;
+console.log(totalPrice);
+
+/* Variable Name type 
+  Camel case - fullName
+  snake case - full_name
+  kabab case - full-name
+  pascal case - FullName */
+
+//var= Variable can re-declare and updated. A global scope variable.
+
+//let= Variable cannot be re-declare but can be updated. A block scope variable.
+
+let fulname="tony stark";
+let age =21;
+let totalPrice=1000;
+console.log(fulname);
+//const= Variable cannot be re-declare or update. A block scope variable. 
