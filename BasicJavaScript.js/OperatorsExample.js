7639c3fe378da0c8774085b83a16d40d3fe35da2
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-05-13 20:41:51 +0530

    Finished Operators type

---
+++ BasicJavaScript.js/OperatorsExample.js
@@ -0,0 +1,25 @@
+/* Types Operators  :-
+1- Addition = '+'
+2- Substraction = '-'
+3- Multiplication = '*'
+4- Division = '/' */
+
+let a =5;
+let b=2;
+console.log(a+b);
+console.log("a+b =",a+b);
+console.log("a+b =",a-b);
+console.log("a * b =", a * b);
+console.log("a / b =", a/b);
+
+// Other Operators:-
+// 1- Modulus - %
+// 2- Exponentiation - **
+
+// Unary Operator:-
+// 1- Increment - a++ = a+1
+// 2- Decrement - a-- = a-1
+// a++ means post Increment
+// ++a means pre Increment
+// a-- means post Decrement
+// --a means pre Decrement
