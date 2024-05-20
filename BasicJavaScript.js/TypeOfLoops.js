e78fa43bcb0e1e1f69b41b12e0de673da18f457b
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-05-20 21:44:51 +0530

    loops completed

--- BasicJavaScript.js/TypeOfLoops.js
+++ BasicJavaScript.js/TypeOfLoops.js
@@ -8,3 +8,50 @@
 for (let i=1; i<=5; i++) {
     console.log("apna college")
 }
+
+//Calculate sum of 1 to n number
+
+let sum =0;
+let n=9;
+for(let i=1; i<=n; i++){
+    sum = sum +1
+}
+console.log("sum =", sum);
+console.log("Loop has ended");
+
+//WHILE LOOP
+let i=1;
+while(i<=5){
+    console.log("i =",i);
+    i++;
+}
+
+
+//DO-WHILE
+let j=1;
+do{
+    console.log("i =",i);
+    i++;
+} while(i<=5);
+
+
+//FOR-OF LOOP
+let str ="JAVASCRIPT";
+length = 0;
+for (let i of str){
+     console.log("i =",i);
+     length++
+}
+console.log("length =",length);
+
+
+//FOR-IN LOOP
+let student ={
+    name: "Rahul Kumar",
+    age:21,
+    cgpa:8.2,
+    isPasses:true
+};
+for(let key in student){
+    console.log("Key=",key,"value=",student[key]);
+}
