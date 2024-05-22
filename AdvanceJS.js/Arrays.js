93fa01edf9e78e58af2104012e758c3fa552c82d
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-05-22 23:50:41 +0530

    a

--- AdvanceJS.js/Arrays.js
+++ AdvanceJS.js/Arrays.js
@@ -17,3 +17,17 @@ console.log(marks[3]=66);
 for (let idx=0; idx < marks.length; idx++){
     console.log(marks[idx]);
 }
+
+//Array methods
+//1-Push : add to end 
+//2- Pop : delete from end & return
+//3- toString : convert array to string
+//4- Concat() : joins multiple arrays & returns result
+let A = ["ab","za","pa"];
+let b = ["an","ni","vi"];
+let c =A.concat(b);
+console.log(c);
+//5- unshift() : add to start
+//6- shift() : delete from start & return
+//7- Slice() : returns the piece of the array  ex- slice(startidx,ensidx)
+//8- Splice() : change original array (add, remove, replace) ex- (startidx, delCount,newEl1...)
