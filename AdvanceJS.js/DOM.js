9e0fc41c3f5e9ad7f81fe5f7c94e78c433cfe404
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-05-27 21:20:14 +0530

    a

---
+++ AdvanceJS.js/DOM.js
@@ -0,0 +1,10 @@
+//Document object model.
+
+//DOM Manipulation Properties
+//1- tagNmae : Return tag for element NodeList.
+ ElementName.tagname
+//2- innerText : return the text content of the element and all its children
+
+//3- innerHTML : return the plain text or HTML contents in the element
+
+//4- textContent : returns textual content even for hidden element
